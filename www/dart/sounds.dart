b4b8a363ea62bb2af370d25c2c0776fff1f13b60
Michael Horn <michael-horn@northwestern.edu>
2015-07-01 00:47:38 -0500

    changed library name

--- www/dart/sounds.dart
+++ www/dart/sounds.dart
@@ -19,7 +19,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-part of StickerBook;
+part of tern2;
 
 class Sounds {
 
--- www/dart/utils.dart
+++ www/dart/utils.dart
@@ -1,12 +1,26 @@
 /*
- * StickerBook
- *
- * Michael S. Horn
- * Northwestern University
- * michael-horn@northwestern.edu
- * Copyright 2013, Michael S. Horn
+ * Tern Tangible Programming Language
+ * Copyright (c) 2013 Michael S. Horn
+ * 
+ *           Michael S. Horn (michael-horn@northwestern.edu)
+ *           Northwestern University
+ *           2120 Campus Drive
+ *           Evanston, IL 60613
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2) as
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-part of StickerBook;
+part of tern2;
 
 
 /**
